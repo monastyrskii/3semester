ce716a69706388a560a81ce507bd06fa411fe816
monastyrskii <44464844+monastyrskii@users.noreply.github.com>
2019-12-10 22:30:19 +0300

    Delete surgeon.java

--- surgeon.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class surgeon implements doctor {
-    private int operations_performed = 0;
-
-    public surgeon(int operations_performed) {
-        this.operations_performed = operations_performed;
-    }
-}
