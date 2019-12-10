07c62187b968ff4dfa3a1ac94974d59e739f2200
monastyrskii <44464844+monastyrskii@users.noreply.github.com>
2019-12-10 19:47:25 +0300

    Add files via upload

---
+++ 1 лаба.cpp
@@ -0,0 +1,236 @@
+﻿//Монастырский Егор, 18-САИ-1, Вариант 7
+
+// Создать шаблон класса «бинарное дерево». Использовать его для сортировки целых чисел и строк, задаваемых с клавиатуры или из файла. 
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+template<class T>
+class bstree {
+	struct node {
+		node* left;
+		node* right;
+		T     val;
+		node(void) noexcept :left(nullptr), right(nullptr) {}
+	};
+private:
+	node* tr;
+public:
+	bstree(void) noexcept :tr(nullptr) {}
+	~bstree() { clear(); }
+
+	bstree(const bstree&) = delete;
+	bstree& operator = (const bstree&) = delete;
+public:
+	//добавить(только для уникальных значений)
+	void add(const T& v) {
+		node* p;
+		if (tr == nullptr) {
+			tr = new node();
+			tr->val = v;
+		}
+		else {
+			node** rp = _find(v);
+			if (tr != nullptr) {
+				p = new node();
+				p->val = v;
+				*rp = p;
+			}
+		}
+	}
+
+	void add(T&& v) {
+		node* p;
+		if (tr == nullptr) {
+			tr = new node();
+			tr->val = forward<T>(v);
+		}
+		else {
+			node** rp = _find(v);
+			if (tr != nullptr) {
+				p = new node();
+				p->val = forward<T>(v);
+				*rp = p;
+			}
+		}
+	}
+
+	//удаление с возвращением min-значения
+	T pop_min(void) noexcept {
+		T v;
+		node** rp = &tr, * p = tr;
+		if (tr != nullptr) {
+			while (p->left != nullptr) {
+				rp = &p->left;
+				p = p->left;
+			}
+		}
+
+		if (p != nullptr) {
+			v = forward<T>(p->val);
+			_erase(rp, p);
+		}
+		return forward<T>(v);
+	}
+
+	//удаление с возвращением max-значения
+	T pop_max(void) noexcept {
+		T v;
+		node** rp = &tr, * p = tr;
+		if (tr != nullptr) {
+			while (p->right != nullptr) {
+				rp = &p->right;
+				p = p->right;
+			}
+		}
+
+		if (p != nullptr) {
+			v = forward<T>(p->val);
+			_erase(rp, p);
+		}
+		return forward<T>(v);
+	}
+
+	//удаление всех
+	void clear(void) noexcept {
+		_clear(tr);
+		tr = nullptr;
+	}
+
+	bool empty(void) const noexcept { return (tr == nullptr); }
+private:
+
+	node** _find(const T& v) noexcept {
+		node** rp = &tr, * p = tr;
+		while (p != nullptr) {
+			if (v == p->val)
+				return nullptr;
+			else if (v < p->val) {
+				rp = &p->left;
+				p = p->left;
+			}
+			else {
+				rp = &p->right;
+				p = p->right;
+			}
+		}
+		return rp;
+	}
+
+	void _erase(node** rp, node* p) noexcept {
+		if (p->right == nullptr)
+			*rp = p->left;
+		else {
+			node* t = p->right;
+			if (t->left == nullptr) {
+				t->left = p->left;
+				*rp = t;
+			}
+			else {
+				node* i = t->left;
+				while (i->left != nullptr) {
+					t = i;
+					i = t->left;
+				}
+				t->left = i->right;
+				i->left = p->left;
+				i->right = p->right;
+				*rp = i;
+			}
+		}
+		delete p;
+	}
+
+	void _clear(node* p) noexcept {
+		if (p != nullptr) {
+			if (p->left != nullptr)
+				_clear(p->left);
+			if (p->right != nullptr)
+				_clear(p->right);
+			delete p;
+		}
+	}
+};
+
+/* чтение массива данных из строки, консоли или файла
+   N = кол-во элементов
+   данные
+   ...
+*/
+template<typename T>
+void get_array(vector<T>& vs, istream& _in) {
+	T v;
+	size_t n;
+	if (!(_in >> n) || !n)
+		return;
+
+	vs.resize(n);
+	for (size_t i = 0; i < n; ++i) {
+		if (!(_in >> vs[i]))
+			break;
+	}
+}
+
+int main(void) {
+	vector<int>::size_type i;
+
+	//сортировка целых чисел
+	vector<int> ia;
+
+	//ввод из консоли
+		get_array<int>(ia, cin);
+
+	bstree<int> ti;
+	for (auto v : ia)
+		ti.add(v);
+
+	for (i = 0; !ti.empty(); ++i)
+		ia[i] = ti.pop_min();
+
+	for (auto v : ia)
+		cout << v << ' ';
+	cout << endl;
+
+	//сортировка строк
+	vector<string> sa;
+
+	//ввод из строки
+	char s[] = "10\n5 4 7 9 8 2 1 3 0 6";
+	istringstream sp(s);
+	get_array<int>(ia, sp);
+
+	bstree<string> ts;
+	for (i = 0; i < sa.size(); ++i)
+		ts.add(move(sa[i]));
+
+	for (i = 0; !ts.empty(); ++i)
+		sa[i] = ts.pop_min();
+
+	for (const auto& v : sa)
+		cout << v << ' ';
+	cout << endl;
+
+	//сортировка действительных чисел по убыванию
+	vector<double> da;
+
+	//ввод из строки
+	char s2[] = "5\n 3.4 -8.5 543.1 1.5 4.4";
+	istringstream sp2(s2);
+	get_array<double>(da, sp2);
+
+	bstree<double> td;
+	for (const auto& d : da)
+		td.add(d);
+
+	for (i = 0; !td.empty(); ++i)
+		da[i] = td.pop_max();
+
+	for (const auto& v : da)
+		cout << v << ' ';
+	cin.get();
+	return 0;
+}
---
+++ 2 лаба.cpp
@@ -0,0 +1,191 @@
+﻿//Монастырский Егор, 18 САИ-1
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+class phcall
+{
+public:
+	char from[100], to[100];
+	int dateandtime, date, time, howlong, ID;
+	int days, months, years, hours, minutes;
+
+	//конструктор
+	phcall(char from[100], char to[100], int days, int months, int years, int hours, int minutes, int howlong)
+	{
+		strcpy(this->from, from);
+		strcpy(this->to, to);
+		this->days = days;
+		this->months = months;
+		this->years = years;
+		this->hours = hours;
+		this->minutes = minutes;
+		this->howlong = howlong;
+		date = days + months * 30 + years * 365;
+		time = hours * 60 + minutes;
+		dateandtime = time + (date - 1) * 1440;
+	}
+	void furcout() {
+		cout << ID << "   " << from << " " << to << " " << days << "." << months << "." << years << "   " << hours << ":" << minutes << "    " << howlong;
+	}
+};
+
+int whatdoyouneed() {
+	int input;
+	cout << "Что вам нужно? \n";
+	cout << "1 - вывести полный список звонков \n";
+	cout << "2 - добавить по номеру \n";
+	cout << "3 - удалить по номеру \n";
+	cout << "4 - вывод с даты по дату \n";
+	cout << "5 - сортировка по давности \n";
+	cout << "6 - сортировка по длительности \n";
+	cout << "7 - вывод от конкретного человека \n";
+	cout << "8 - вывод к конкретному человеку \n";
+
+	cin >> input;
+	return input;
+}
+
+
+
+int main()
+{
+	vector<phcall> callbook;
+	vector<phcall>::const_iterator it = callbook.begin();
+
+	ifstream inputfile; 	//открываем файл
+	inputfile.exceptions(ifstream::badbit | ifstream::failbit);
+
+	int i(1), number, day, month, year, hour, minute, howlong, ID;
+	char from[100], to[100];
+
+	try {
+		inputfile.open("INPUT.TXT");
+		cout << "Файл открыт" << endl;
+		while (i) { 		//считываем данные из файла
+			inputfile >> number;
+			if (number == i) {
+				inputfile >> from >> to >> day >> month >> year >> hour >> minute >> howlong;
+				phcall addition(from, to, day, month, year, hour, minute, howlong);
+				addition.ID = i;
+				callbook.push_back(addition);
+				i++;
+			}
+			else i = 0;
+		}
+
+	}
+	catch (const ifstream::failure & ex) {
+		cout << ex.what() << endl;
+		cout << ex.code() << endl;
+		cout << "Ошибка. Возможно файл был удалён или перемещён" << endl;
+	}
+
+	int input = whatdoyouneed();
+
+	if (input == 1) {		// 1. Вывести полный список звонков
+		for (i = 0; i < callbook.size(); i++) {
+			callbook.at(i).furcout();
+		}
+		input = whatdoyouneed();
+	}
+
+	if (input == 2) { 	// 2. Добавить по номеру
+		cout << "Введите номер, который вы хотите добавить \n";
+		cin >> input;
+		cout << "Введите нужные данные: \n";
+		cin >> from >> to >> day >> month >> year >> hour >> minute >> howlong >> ID;
+		phcall addition(from, to, day, month, year, hour, minute, howlong);
+		addition.ID = ID;
+		advance(it, input);
+		callbook.insert(it, addition);
+		it = callbook.begin();
+	}
+
+	else if (input == 3) {	// 3. Удалить по номеру
+		cout << "Введите номер, котороый вы хотите удалить \n";
+		cin >> input;
+		advance(it, input);
+		callbook.erase(it);
+		it = callbook.begin();
+	}
+
+	else if (input == 4) { 	// 4. Вывод с даты по дату
+		int mindate, maxdate;
+		cout << "Введите минимальную дату (dd mm yy) \n";
+		cin >> day >> month >> year;
+		mindate = day + month * 30 + year * 365;
+		cout << "Введите максимальную дату (dd mm yy) \n";
+		cin >> day >> month >> year;
+		maxdate = day + month * 30 + year * 365;
+
+		cout << "Результаты отбора по дате \n";
+		for (i = 0; i < callbook.size(); i++) {
+			if (callbook[i].date >= mindate && callbook[i].date <= maxdate) {
+				callbook.at(i).furcout();
+			}
+		}
+	}
+
+	else if (input == 5) { 	// 5. Сортировка по дате и времени
+		cout << "Сортировка по дате: \n";
+		sort(callbook.begin(), callbook.end(), [](const phcall& a, const phcall& b) {
+			return a.date < b.date;
+			});
+		for (i = 0; i < callbook.size(); i++) {
+			callbook.at(i).furcout();
+		}
+		cout << "\nСортировка по времени: \n";
+		sort(callbook.begin(), callbook.end(), [](const phcall& a, const phcall& b) {
+			return a.time < b.time;
+			});
+		for (i = 0; i < callbook.size(); i++) {
+			callbook.at(i).furcout();
+		}
+		cout << "\nСортировка по дате и времени: \n";
+		sort(callbook.begin(), callbook.end(), [](const phcall& a, const phcall& b) {
+			return a.dateandtime < b.dateandtime;
+			});
+		for (i = 0; i < callbook.size(); i++) {
+			callbook.at(i).furcout();
+		}
+	}
+
+	else if (input == 6) { 	// 6. Сортировка по длительности
+		cout << "\nСортировка по длительности: \n";
+		sort(callbook.begin(), callbook.end(), [](const phcall& a, const phcall& b) {
+			return a.howlong < b.howlong;
+			});
+		for (i = 0; i < callbook.size(); i++) {
+			callbook.at(i).furcout();
+		}
+		input = whatdoyouneed();
+	}
+
+	else if (input == 7) { 	// 7. Вывод от конкретного человека
+		cout << "Введите имя человека для поиска звонков от него \n";
+		cin >> from;
+		for (i = 0; i < callbook.size(); i++) {
+			if (callbook.at(i).from == from) {
+				callbook.at(i).furcout();
+			}
+		}
+	}
+
+	else if (input == 8) { 	// 8. вывод к конкретному человеку
+		cout << "Введите имя человека для поиска звонков к нему \n";
+		cin >> to;
+		for (i = 0; i < callbook.size(); i++) {
+			if (callbook.at(i).to == to) {
+				callbook.at(i).furcout();
+			}
+		}
+	}
+
+	return 0;
+
+}
